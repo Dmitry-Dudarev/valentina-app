d36abb2dbb3b8efc518bc8c72f2d05bedc20072c
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-11-23 18:54:17 +0300

    feat: гачата разработка header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header(props) {
+  return(
+    <header className="header">
+      <div className="header__navigation">
+
+      </div>
+    </header>
+  )
+}
