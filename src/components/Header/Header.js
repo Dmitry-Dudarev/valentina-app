e7fb176729fefe372b740c7b17e878bedf832e6c
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-11-25 15:04:12 +0300

    feat: работа наз header завершена

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import HeaderLogo from "../../images/header__logo.svg";
 import { headerText } from "../../constants/header-text";
 import "./Header.css";
 
 function Header(props) {
+  // получаем текущую локацию пользователя
+  const currentLocation = useLocation().pathname;
+  // формируем сообщение пользователю о его
+  // текущем положении на сайте
+  // на главной странице сообщения нет
+  const location = (
+    currentLocation === "/about-me" ? headerText.aboutMe : 
+    currentLocation === "/portfolio" ? headerText.portfolio :
+    currentLocation === "/services" ? headerText.services :
+    currentLocation === "/contacts" ? headerText.contacts :
+    "");
+
   return (
     <header className="header">
       <Link className="app-link header__main-link" to={"/"}>
         <img className="header__logo" src={HeaderLogo} alt="логотип" />
       </Link>
       
+      <p className="app-text header__location">{location}</p>
+
       <div className="header__navigation">
         <Link className="app-link app-text header__navigation-link" to={"/about-me"}>
           {headerText.aboutMe.toUpperCase()}
