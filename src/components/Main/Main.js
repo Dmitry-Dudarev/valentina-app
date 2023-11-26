ceb564485b2df4958e976975c2c186f92bd0ffaf
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-11-26 16:16:54 +0300

    feat: добавлены изображения

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,14 +7,10 @@ import { linksText } from "../../constants/links-text";
 function Main(props) {
   return (
     <section className="main">
-      <div className="main__banner">
-        <h1 className="app-text main__banner-text">
-          Баннер на главной странице
-        </h1>
-      </div>
+      <div className="main__banner" />
       <div className="main__content">
         <div className="main__grid">
-          <div className="main__grid-cell main__grid-banner-left">левый баннер</div>
+          <div className="main__grid-cell main__grid-banner-left" />
           <div className="main__grid-cell main__grid-banner-text">
             <p className="app-text main__banner-text main__grid-banner-text-paragraph-first">
               {mainText.textBlockParagraphFirst}
@@ -27,7 +23,7 @@ function Main(props) {
               {linksText.contacts.toUpperCase()}
             </Link>
           </div>
-          <div className="main__grid-cell main__grid-banner-right">правый баннер</div>
+          <div className="main__grid-cell main__grid-banner-right" />
 
           <Link className="app-link main__grid-cell main__grid-about-me-link" to={"/about-me"}>
             <h2 className="app-text main__grid-cell-title">{linksText.aboutMe.toUpperCase()}</h2>
@@ -35,8 +31,10 @@ function Main(props) {
           </Link>
 
           <Link className="app-link main__grid-cell main__grid-portfolio-link" to={"/portfolio"}>
-            <h2 className="app-text main__grid-cell-title">{linksText.portfolio.toUpperCase()}</h2>
-            <div className="main__grid-cell-image" />
+            <h2 className="app-text main__grid-cell-title main__grid-portfolio-link-title">
+              {linksText.portfolio.toUpperCase()}
+            </h2>
+            <div className="main__grid-cell-image main__grid-portfolio-cell-image" />
           </Link>
 
           <Link className="app-link main__grid-cell main__grid-services-link" to={"/services"}>
