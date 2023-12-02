d90e66b61c8039d6133e37dc325f34500f36ecf8
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-12-03 00:59:26 +0300

    feat: начата разработка секции medium-links

---
+++ src/components/Medium-links/Medium-links.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { linksText } from "../../constants/links-text";
+
+function MediumLinks(props) {
+  return (
+    <div className="medium-links">
+      <Link className="medium-links__unit medium-links__about-me" to={"/about-me"}>
+        <h2 className="app-text medium-links__title">
+          {linksText.aboutMe.toUpperCase()}
+        </h2>
+        <div className="medium-links__content">
+
+        </div>
+      </Link>
+    </div>
+  )
+}
